a615bab57397d1d7b4041fca17663c1f5a3845ac
calumbrash <calumbrash@gmail.com>
2017-12-22 12:11:05 +1300

    Small fix to top notification.

--- dist/js/shared-ui.js
+++ dist/js/shared-ui.js
@@ -336,13 +336,14 @@
 
 (function ($) {
 
+	$('.sui-notice-top:not(.sui-cant-dismiss)').delay(3000).slideUp('slow');
 	$( '.sui-notice-dismiss' ).click( function( e ) {
 		e.preventDefault();
-		$(this).parent( '.sui-notice' ).slideUp('slow');
+		$(this).parent( '.sui-notice' ).stop().slideUp('slow');
+		return false;
 	});
 
 
-	$('.sui-notice-top:not(.sui-cant-dismiss)').delay(3000).slideUp('slow');
 
 
 }(jQuery));
