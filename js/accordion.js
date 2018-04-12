86530e7c4cfbf7d300db936e35729aadef844a28
calumbrash <calumbrash@gmail.com>
2018-04-12 17:23:58 +1200

    [Enhance] Make Accordion function callable.

--- js/accordion.js
+++ js/accordion.js
@@ -1,20 +1,40 @@
 ( function( $ ) {
 
-	var accordionTable = $( 'SUI_BODY_CLASS .sui-accordion' );
+    // Enable strict mode.
+    'use strict';
 
-	accordionTable.on( 'click', '.sui-accordion-item', function() {
+    // Define global SUI object if it doesn't exist.
+    if ( 'object' !== typeof window.SUI ) {
+        window.SUI = {};
+    }
 
-		var getParentItem = $( this ).closest( '.sui-accordion-item' ),
-			getNextAdditionalContentRow = getParentItem.nextUntil( '.sui-accordion-item' );
+    SUI.suiAccordion = function( el ) {
+        var accordionTable = $( el );
 
-		getNextAdditionalContentRow.toggleClass( 'sui-accordion-item--open' );
+        function init() {
+            accordionTable.on( 'click', '.sui-accordion-item', function() {
 
-		if ( getNextAdditionalContentRow.hasClass( 'sui-accordion-item--open' ) ) {
-			getParentItem.addClass( 'sui-accordion-item--open' );
-		} else {
-			getParentItem.removeClass( 'sui-accordion-item--open' );
-		}
+                var getParentItem = $( this ).closest( '.sui-accordion-item' ),
+                    getNextAdditionalContentRow = getParentItem.nextUntil( '.sui-accordion-item' );
 
-	});
+                getNextAdditionalContentRow.toggleClass( 'sui-accordion-item--open' );
 
+                if ( getNextAdditionalContentRow.hasClass( 'sui-accordion-item--open' ) ) {
+                    getParentItem.addClass( 'sui-accordion-item--open' );
+                } else {
+                    getParentItem.removeClass( 'sui-accordion-item--open' );
+                }
+
+            });
+        }
+
+        init();
+
+        return this;
+    };
+
+    // Convert all accordions.
+    $( 'SUI_BODY_CLASS .sui-accordion' ).each( function() {
+        SUI.suiAccordion( this );
+    });
 }( jQuery ) );
