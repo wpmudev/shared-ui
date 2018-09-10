2080a6dffc3c2c49ae94a42b6608832e0b360055
Leighton <shiasapir@gmail.com>
2018-09-10 14:03:46 -0500

    [fix] prevent accordion to open on button click

--- js/accordion.js
+++ js/accordion.js
@@ -1,56 +1,60 @@
-
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;
 ( function( $, window, document, undefined ) {
 
-    'use strict';
+	'use strict';
 
-    // undefined is used here as the undefined global variable in ECMAScript 3 is
-    // mutable (ie. it can be changed by someone else). undefined isn't really being
-    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-    // can no longer be modified.
+	// undefined is used here as the undefined global variable in ECMAScript 3 is
+	// mutable (ie. it can be changed by someone else). undefined isn't really being
+	// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+	// can no longer be modified.
 
-    // window and document are passed through as local variables rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
+	// window and document are passed through as local variables rather than global
+	// as this (slightly) quickens the resolution process and can be more efficiently
+	// minified (especially when both are regularly referenced in your plugin).
 
-    // Create the defaults once
-    var pluginName = 'SUIAccordion',
-        defaults = {};
+	// Create the defaults once
+	var pluginName = 'SUIAccordion',
+		defaults = {};
 
-    // The actual plugin constructor
-    function SUIAccordion( element, options ) {
-        this.element = element;
-        this.$element = $( this.element );
+	// The actual plugin constructor
+	function SUIAccordion( element, options ) {
+		this.element = element;
+		this.$element = $( this.element );
 
 
-        // jQuery has an extend method which merges the contents of two or
-        // more objects, storing the result in the first object. The first object
-        // is generally empty as we don't want to alter the default options for
-        // future instances of the plugin
-        this.settings = $.extend({}, defaults, options );
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
+		// jQuery has an extend method which merges the contents of two or
+		// more objects, storing the result in the first object. The first object
+		// is generally empty as we don't want to alter the default options for
+		// future instances of the plugin
+		this.settings = $.extend({}, defaults, options );
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
 
-    // Avoid Plugin.prototype conflicts
-    $.extend( SUIAccordion.prototype, {
-        init: function() {
+	// Avoid Plugin.prototype conflicts
+	$.extend( SUIAccordion.prototype, {
+		init: function() {
+			var self = this;
 
-            // namespaced event
-            this.$element.on( 'click.sui.accordion', 'div.sui-accordion-item-header, tr.sui-accordion-item', function( event ) {
+			// namespaced event
+			this.$element.on( 'click.sui.accordion', 'div.sui-accordion-item-header, tr.sui-accordion-item', function( event ) {
 
 				var getItem = $( this ).closest( '.sui-accordion-item' ),
 					getContent = getItem.nextUntil( '.sui-accordion-item' ).filter( '.sui-accordion-item-content' ),
-					getParent = getItem.closest( '.sui-accordion' )
+					getParent = getItem.closest( '.sui-accordion' ),
+					clickedTarget = $( event.target )
 					;
+
+				if ( clickedTarget.closest( '.sui-accordion-item-actions' ).length ) {
+					return true;
+				}
 
 				if ( getParent.hasClass( 'sui-table' ) ) {
 
 					if ( ! getItem.hasClass( 'sui-accordion-item--disabled' ) ) {
-
 						getContent.toggleClass( 'sui-accordion-item--open' );
 
 						if ( getContent.hasClass( 'sui-accordion-item--open' ) ) {
@@ -66,45 +70,45 @@
 					}
 				}
 
-            });
-        }
-    });
+			});
+		}
+	});
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[ pluginName ] = function( options ) {
-        return this.each( function() {
+	// A really lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[ pluginName ] = function( options ) {
+		return this.each( function() {
 
-            // instance of SUIAccordion can be called with $(element).data('SUIAccordion')
-            if ( ! $.data( this, pluginName ) ) {
-                $.data( this, pluginName, new SUIAccordion( this, options ) );
-            }
-        });
-    };
+			// instance of SUIAccordion can be called with $(element).data('SUIAccordion')
+			if ( ! $.data( this, pluginName ) ) {
+				$.data( this, pluginName, new SUIAccordion( this, options ) );
+			}
+		});
+	};
 
 }( jQuery, window, document ) );
 
 ( function( $ ) {
 
-    // Enable strict mode.
-    'use strict';
+	// Enable strict mode.
+	'use strict';
 
-    // Define global SUI object if it doesn't exist.
-    if ( 'object' !== typeof window.SUI ) {
-        window.SUI = {};
-    }
+	// Define global SUI object if it doesn't exist.
+	if ( 'object' !== typeof window.SUI ) {
+		window.SUI = {};
+	}
 
-    SUI.suiAccordion = function( el ) {
-        var accordionTable = $( el );
+	SUI.suiAccordion = function( el ) {
+		var accordionTable = $( el );
 
-        function init() {
-            accordionTable.SUIAccordion({});
-        }
+		function init() {
+			accordionTable.SUIAccordion({});
+		}
 
-        init();
+		init();
 
-        return this;
-    };
+		return this;
+	};
 
 	if ( 0 !== $( 'SUI_BODY_CLASS .sui-accordion' ).length ) {
 
