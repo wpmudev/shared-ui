b381f13c3a91cf8a00c0a014912798c30022e8fe
calumbrash <calumbrash@gmail.com>
2018-11-26 13:05:33 +1300

    Enhance - Tabs
    Removed JS state save of selected tab.
    Add ability to show tab when hash from url matches tab id.

--- js/tabs.js
+++ js/tabs.js
@@ -8,7 +8,9 @@
         window.SUI = {};
     }
 
-    SUI.suiTabs = function( options ) {
+    SUI.suiTabs = ( function () {
+        var fn = {};
+
         var data;
         var types = [ 'tab', 'pane' ];
         var type;
@@ -20,155 +22,104 @@
         var indexItem;
         var memory = [];
 
-        function init( options ) {
-            var itemIndex;
-            var groupIndex;
-            var tabItems;
+        fn.init = function(options) {
             data = options;
-            setDefaults();
-            setMemory();
+            fn.setDefaults();
 
-            groups.tab = document.querySelectorAll( data.tabGroup );
-            groups.pane = document.querySelectorAll( data.paneGroup );
+            groups['tab'] = document.querySelectorAll( data.tabGroup );
+            groups['pane'] = document.querySelectorAll( data.paneGroup );
 
-            for ( groupIndex = 0; groupIndex < groups.tab.length; groupIndex++ ) {
-                tabItems = groups.tab[groupIndex].children;
+            for( var groupIndex = 0; groupIndex < groups['tab'].length; groupIndex++ ) {
+                var tabItems = groups['tab'][groupIndex].children;
 
-                for ( itemIndex = 0; itemIndex < tabItems.length; itemIndex++ ) {
-                    tabItems[itemIndex].addEventListener( 'click', onClick.bind( this, groupIndex, itemIndex ), false );
+                for( var itemIndex = 0; itemIndex < tabItems.length; itemIndex++ ) {
+                    tabItems[itemIndex].addEventListener('click', fn.onClick.bind(this, groupIndex, itemIndex), false);
 
                     indexGroup = groupIndex;
                     indexItem = itemIndex;
 
-                    if ( ! hasMemory() ) {
-                        continue;
+                    if ( window.location.hash ) {
+                        var hashId = window.location.hash.replace( /[^\w-_]/g, '' );
+
+                        if ( hashId === tabItems[itemIndex].id ) {
+                            fn.setNodes(groupIndex, itemIndex);
+                        }
                     }
-                    setNodes( groupIndex, itemIndex );
                 }
+
             }
-        }
+        };
 
-        function onClick( groupIndex, itemIndex ) {
-            setNodes( groupIndex, itemIndex );
+        fn.onClick = function(groupIndex, itemIndex) {
+            fn.setNodes(groupIndex, itemIndex);
 
-            setCallback( indexGroup, indexItem );
-        }
+            fn.setCallback(indexGroup, indexItem);
+        };
 
-        function setNodes( groupIndex, itemIndex ) {
-            var i;
+        fn.setNodes = function(groupIndex, itemIndex) {
             indexGroup = groupIndex;
             indexItem = itemIndex;
 
-            for ( i = 0; i < types.length; i++ ) {
+            for( var i = 0; i < types.length; i++ ) {
                 type = types[i];
 
-                setActiveGroup();
-                setActiveChildren();
-                setActiveItems();
-                putActiveClass();
+                fn.setActiveGroup();
+                fn.setActiveChildren();
+                fn.setActiveItems();
+                fn.putActiveClass();
             }
 
             memory[groupIndex] = [];
             memory[groupIndex][itemIndex] = true;
 
-            localStorage.setItem( 'tabbis', JSON.stringify( memory ) );
-        }
+        };
 
-        function hasMemory() {
-            try {
-                if ( 'undefined' === typeof memory ) {
-                    return;
-                }
-                if  ( 'undefined' === typeof memory[indexGroup]) {
-                    return;
-                }
-                if ( 'undefined' === typeof memory[indexGroup][indexItem]) {
-                    return;
-                }
-                if ( true !== memory[indexGroup][indexItem]) {
-                    return;
-                }
-            } catch ( e ) {
-                localStorage.removeItem( 'tabbis' );
-                return;
+        fn.putActiveClass = function() {
+            for( var i = 0; i < activeChildren[type].length; i++ ) {
+                activeChildren[type][i].classList.remove(data[type + 'Active']);
             }
 
-            return true;
-        }
+            activeItems[type].classList.add(data[type + 'Active']);
+        };
 
-        function setMemory() {
-            if ( null === localStorage.getItem( 'tabbis' ) ) {
-                return;
-            }
-            if ( 0 === localStorage.getItem( 'tabbis' ).length ) {
-                return;
-            }
-
-            memory = Object.values( JSON.parse( localStorage.getItem( 'tabbis' ) ) );
-        }
-
-        function putActiveClass() {
-            var i;
-            for ( i = 0; i < activeChildren[type].length; i++ ) {
-                activeChildren[type][i].classList.remove( data[type + 'Active']);
-            }
-
-            activeItems[type].classList.add( data[type + 'Active']);
-        }
-
-        function setDefaults() {
-            var i;
-            for ( i = 0; i < types.length; i++ ) {
+        fn.setDefaults = function() {
+            for( var i = 0; i < types.length; i++ ) {
                 type = types[i];
 
-                setOption( type + 'Group', '[data-' + type + 's]' );
-                setOption( type + 'Active', 'active' );
+                fn.setOption(type + 'Group', '[data-' + type + 's]');
+                fn.setOption(type + 'Active', 'active');
             }
-        }
+        };
 
-        function setOption( key, value ) {
+        fn.setOption = function(key, value) {
             data = data || [];
             data[key] = data[key] || value;
-        }
+        };
 
-        function setActiveGroup() {
+        fn.setActiveGroup = function() {
             activeGroups[type] = groups[type][indexGroup];
-        }
+        };
 
-        function setActiveChildren() {
+        fn.setActiveChildren = function() {
             activeChildren[type] = activeGroups[type].children;
-        }
+        };
 
-        function setActiveItems() {
+        fn.setActiveItems = function() {
             activeItems[type] = activeChildren[type][indexItem];
-        }
+        };
 
-        function setCallback() {
-            if ( 'function' === typeof data.callback ) {
-                data.callback( activeItems.tab, activeItems.pane );
+        fn.setCallback = function() {
+            if (typeof data.callback === "function") {
+                data.callback(activeItems.tab, activeItems.pane);
             }
-        }
+        };
 
-        function reset() {
-            var groupIndex;
-            var itemIndex;
-            for ( groupIndex = 0;  groupIndex < groups.tab.length; groupIndex++ ) {
-                tabItems = groups.tab[groupIndex].children;
-                paneItems = groups.pane[groupIndex].children;
+        return fn;
+    } )();
 
-                for ( itemIndex = 0; itemIndex < tabItems.length; itemIndex++ ) {
-                    tabItems[itemIndex].classList.remove( data['tab-active']);
-                    paneItems[itemIndex].classList.remove( data['pane-active']);
-                }
-            }
-            localStorage.removeItem( 'tabbis' );
-        }
-
-        init( options );
-    };
 
     if ( 0 !== $( 'SUI_BODY_CLASS .sui-tabs' ).length ) {
-        SUI.suiTabs();
+        SUI.suiTabs.init();
     }
 
 }( jQuery ) );
