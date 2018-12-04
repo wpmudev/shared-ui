c64041ba9bffaae69d15cc32d1b3c06dd8cbeffe
calumbrash <calumbrash@gmail.com>
2018-12-04 18:31:04 +1300

    Build JS and CSS.

--- dist/js/shared-ui.js
+++ dist/js/shared-ui.js
@@ -8585,13 +8585,21 @@
 ( function( $ ) {
 
 	// Convert all select lists to fancy sui Select lists.
-	$( '.sui-select' ).SUIselect2({
-		dropdownCssClass: 'sui-select-dropdown'
-	});
-
-	$( '.sui-variables' ).SUIselect2({
-		dropdownCssClass: 'sui-variables-dropdown'
-	});
+    if ( $( '.sui-color-accessible' )[0]) {
+        $( '.sui-select' ).SUIselect2({
+            dropdownCssClass: 'sui-select-dropdown sui-color-accessible'
+        });
+        $( '.sui-variables' ).SUIselect2({
+            dropdownCssClass: 'sui-variables-dropdown sui-color-accessible'
+        });
+    } else {
+        $( '.sui-select' ).SUIselect2({
+            dropdownCssClass: 'sui-select-dropdown'
+        });
+        $( '.sui-variables' ).SUIselect2({
+            dropdownCssClass: 'sui-variables-dropdown'
+        });
+    }
 
 }( jQuery ) );
 
