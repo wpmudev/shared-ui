8b096c985e4809f34f013e51f62853e24b14b922
Calum Brash <calum@incsub.com>
2019-02-27 11:48:14 +1300

    :package:

--- dist/js/_src/scores.js
+++ dist/js/_src/scores.js
@@ -26,7 +26,7 @@
 		$( el ).prepend( svg ).addClass( 'loaded' ).find( 'circle:last-child' ).css( 'animation', 'sui' + score + ' 3s forwards' );
 	};
 
-	$( '.sui-2-3-19 .sui-circle-score' ).each( function() {
+	$( '.sui-2-3-20 .sui-circle-score' ).each( function() {
 		SUI.loadCircleScore( this );
 	});
 
