d8b8e1994f744de2eeb47c28c3a8de9977866264
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2019-04-24 01:10:21 -0500

    [New] Side tabs function.

---
+++ js/side-tabs.js
@@ -0,0 +1,44 @@
+( function( $ ) {
+
+	// Enable strict mode
+	'use strict';
+
+	// Define global SUI object if it doesn't exist
+	if ( 'object' !== typeof window.SUI ) {
+		window.SUI = {};
+	}
+
+	SUI.sideTabs = function( element ) {
+
+		var $this 	   = $( element ),
+			$label     = $this.parent( 'label' ),
+			$data      = $this.data( 'tab-menu' ),
+			$wrapper   = $this.closest( '.sui-side-tabs' ),
+			$alllabels = $wrapper.find( '>.sui-tabs-menu .sui-tab-item' ),
+			$allinputs = $alllabels.find( 'input' ),
+			newContent
+			;
+
+		$this.on( 'click', function( e ) {
+
+			$alllabels.removeClass( 'active' );
+			$allinputs.removeAttr( 'checked' );
+			$wrapper.find( '.sui-tabs-content>div[data-tab-content]' ).removeClass( 'active' );
+
+			$label.addClass( 'active' );
+			$this.attr( 'checked', 'checked' );
+
+			newContent = $wrapper.find( '.sui-tabs-content div[data-tab-content="' + $data + '"]' );
+
+			if ( newContent.length ) {
+				newContent.addClass( 'active' );
+			}
+		});
+
+	};
+
+	$( 'SUI_BODY_CLASS .sui-side-tabs label.sui-tab-item input' ).each( function() {
+		SUI.sideTabs( this );
+	});
+
+}( jQuery ) );
