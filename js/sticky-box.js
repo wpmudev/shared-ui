bcc66870d51dbc0c8d90e7844d686a09eb34b026
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2019-09-26 22:12:15 -0500

    [update] remove spacing fixes to focus on task fix only

--- js/sticky-box.js
+++ js/sticky-box.js
@@ -3,35 +3,31 @@
 	// Enable strict mode.
 	'use strict';
 
-	var _$stickies = [].slice.call( document.querySelectorAll( '.sui-box-sticky' ) );
+    var _$stickies = [].slice.call(document.querySelectorAll('.sui-box-sticky'));
 
-    _$stickies.forEach( function( _$sticky ) {
-
-        if ( CSS.supports && CSS.supports( 'position', 'sticky' ) ) {
-
+    _$stickies.forEach(function(_$sticky){
+        if (CSS.supports && CSS.supports('position', 'sticky')) {
 			if ( null !== _$sticky.offsetParent ) {
-				apply_sticky_class( _$sticky );
+				apply_sticky_class(_$sticky);
 			}
 
-			window.addEventListener( 'scroll', function() {
-
-				if ( null !== _$sticky.offsetParent ) {
-					apply_sticky_class( _$sticky );
+            window.addEventListener('scroll', function(){
+                if ( null !== _$sticky.offsetParent ) {
+					apply_sticky_class(_$sticky);
 				}
-			});
+            })
         }
     });
 
-    function apply_sticky_class( _$sticky ) {
-
+    function apply_sticky_class(_$sticky){
         var currentOffset = _$sticky.getBoundingClientRect().top;
-        var stickyOffset  = parseInt( getComputedStyle(_$sticky).top.replace( 'px', '' ) );
-        var isStuck       = currentOffset <= stickyOffset;
+        var stickyOffset = parseInt( getComputedStyle(_$sticky).top.replace('px','') );
+        var isStuck = currentOffset <= stickyOffset;
 
-        if ( isStuck ) {
-            _$sticky.classList.add( 'sui-is-sticky' );
+        if (isStuck) {
+            _$sticky.classList.add('sui-is-sticky');
         } else {
-            _$sticky.classList.remove( 'sui-is-sticky' );
+            _$sticky.classList.remove('sui-is-sticky');
         }
     }
 }( jQuery ) );
