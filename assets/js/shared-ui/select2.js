023f697acc49149c6bcfe7f72322d9c0a5af23db
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-09-16 00:20:31 -0500

    [update] [select2] show icon / color on dropdown option.

--- assets/js/shared-ui/select2.js
+++ assets/js/shared-ui/select2.js
@@ -1,5 +1,74 @@
 ( function( $ ) {
 
+	let formatOption = ( data, container ) => {
+
+		let markup, border;
+
+		const label = data.text;
+		const icon  = $( data.element ).attr( 'data-icon' );
+		const color = $( data.element ).attr( 'data-color' );
+
+		if ( ! data.id ) {
+			return label; // optgroup.
+		}
+
+		if ( 'undefined' !== typeof icon ) {
+			markup = '<span class="sui-icon-' + icon.toLowerCase() + '" aria-hidden="true"></span> ' + label;
+		} else if ( 'undefined' !== typeof color ) {
+
+			switch ( color ) {
+
+				case '#FFF' :
+				case 'white' :
+				case '#FFFFFF' :
+					border = '#000';
+					break;
+
+				default :
+					border = color;
+					break;
+			}
+
+			markup = '<span class="sui-color" style="border-color: ' + border + '; background-color: ' + color + ';" aria-hidden="true"></span> ' + label;
+		} else {
+			markup = label;
+		}
+
+		return markup;
+
+	};
+
+	let formatOptionSelection = ( data, container ) => {
+
+		let markup;
+
+		const label = data.text;
+		const color = $( data.element ).attr( 'data-color' );
+
+		if ( 'undefined' !== typeof color ) {
+
+			switch ( color ) {
+
+				case '#FFF' :
+				case 'white' :
+				case '#FFFFFF' :
+					border = '#000';
+					break;
+
+				default :
+					border = color;
+					break;
+			}
+
+			markup = '<span class="sui-color" style="border-color: ' + border + '; background-color: ' + color + ';" aria-hidden="true"></span> ' + label;
+		} else {
+			markup = label;
+		}
+
+		return markup;
+
+	};
+
 	// Convert all select lists to fancy sui Select lists.
     if ( $( '.sui-color-accessible' )[0]) {
         $( '.sui-select' ).SUIselect2({
@@ -24,7 +93,12 @@
 			placeholder: function() {
 				$( this ).data( 'placeholder' );
 			},
-            dropdownCssClass: 'sui-select-dropdown'
+			dropdownCssClass: 'sui-select-dropdown sui-wrap',
+			templateResult: formatOption,
+			templateSelection: formatOptionSelection,
+			escapeMarkup: function( markup ) {
+				return markup;
+			}
 		});
 		$( '.sui-search' ).SUIselect2({
 			placeholder: function() {
