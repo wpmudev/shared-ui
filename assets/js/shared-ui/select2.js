179ae870ee99fca7c226a67030b37c8967bc7f25
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-09-16 23:40:22 -0500

    [update] [select2] load select based on data attributes

--- assets/js/shared-ui/select2.js
+++ assets/js/shared-ui/select2.js
@@ -1,6 +1,14 @@
 ( function( $ ) {
 
-	let formatOption = ( data, container ) => {
+	// Define global SUI object if it doesn't exist.
+	if ( 'object' !== typeof window.SUI ) {
+		window.SUI = {};
+	}
+
+	// Load SUIselect2 function.
+	SUI.loadSelect = SUI.loadSelect || true;
+
+	let formatIcon = ( data, container ) => {
 
 		let markup, border;
 
@@ -38,7 +46,54 @@
 
 	};
 
-	let formatOptionSelection = ( data, container ) => {
+	let formatIconSelection = ( data, container ) => {
+
+		let markup;
+
+		const label = data.text;
+
+		markup = label;
+
+		return markup;
+
+	};
+
+	let formatColor = ( data, container ) => {
+
+		let markup, border;
+
+		const label = data.text;
+		const color = $( data.element ).attr( 'data-color' );
+
+		if ( ! data.id ) {
+			return label; // optgroup.
+		}
+
+		if ( 'undefined' !== typeof color ) {
+
+			switch ( color ) {
+
+				case '#FFF' :
+				case 'white' :
+				case '#FFFFFF' :
+					border = '#000';
+					break;
+
+				default :
+					border = color;
+					break;
+			}
+
+			markup = '<span class="sui-color" style="border-color: ' + border + '; background-color: ' + color + ';" aria-hidden="true"></span> ' + label;
+		} else {
+			markup = label;
+		}
+
+		return markup;
+
+	};
+
+	let formatColorSelection = ( data, container ) => {
 
 		let markup;
 
@@ -69,36 +124,143 @@
 
 	};
 
-	// Convert all select lists to fancy sui Select lists.
-    if ( $( '.sui-color-accessible' )[0]) {
-        $( '.sui-select' ).SUIselect2({
-            dropdownCssClass: 'sui-select-dropdown sui-color-accessible'
+	let formatVars = ( data, container ) => {
+
+		let markup;
+
+		const label = data.text;
+
+		if ( ! data.id ) {
+			return label; // optgroup.
+		}
+
+		markup = label;
+
+		return markup;
+
+	};
+
+	let formatVarsSelection = ( data, container ) => {
+
+		let markup;
+
+		const label = data.text;
+
+		markup = label;
+
+		return markup;
+
+	};
+
+	if ( SUI.loadSelect ) {
+
+		// Default.
+		$( '.sui-select:not([data-search="true"]):not([data-theme])' ).SUIselect2({
+			minimumResultsForSearch: Infinity
 		});
-		$( '.sui-search' ).SUIselect2({
-			minimumInputLength: 2,
-			maximumSelectionLength: 1,
-            dropdownCssClass: 'sui-search-dropdown sui-color-accessible'
+
+		// Default + search input.
+		$( '.sui-select[data-search="true"]:not([data-theme])' ).SUIselect2();
+
+		// Select with icons.
+		$( '.sui-select[data-theme="icon"]:not([data-search="true"])' ).SUIselect2({
+			templateResult: formatIcon,
+			templateSelection: formatIconSelection,
+			escapeMarkup: function( markup ) {
+				return markup;
+			},
+			minimumResultsForSearch: Infinity
 		});
-		$( '.sui-variables' ).SUIselect2({
-            dropdownCssClass: 'sui-variables-dropdown sui-color-accessible'
-		});
-    } else {
-        $( '.sui-select' ).SUIselect2({
-			dropdownCssClass: 'sui-select-dropdown sui-wrap',
-			templateResult: formatOption,
-			templateSelection: formatOptionSelection,
+
+		// Select with icons + search input.
+		$( '.sui-select[data-theme="icon"][data-search="true"]' ).SUIselect2({
+			templateResult: formatIcon,
+			templateSelection: formatIconSelection,
 			escapeMarkup: function( markup ) {
 				return markup;
 			}
 		});
-		$( '.sui-search' ).SUIselect2({
+
+		// Select with colors.
+		$( '.sui-select[data-theme="color"]:not([data-search="true"])' ).SUIselect2({
+			templateResult: formatColor,
+			templateSelection: formatColorSelection,
+			escapeMarkup: function( markup ) {
+				return markup;
+			},
+			minimumResultsForSearch: Infinity
+		});
+
+		// Select with colors + search input.
+		$( '.sui-select[data-theme="color"][data-search="true"]' ).SUIselect2({
+			templateResult: formatColor,
+			templateSelection: formatColorSelection,
+			escapeMarkup: function( markup ) {
+				return markup;
+			}
+		});
+
+		// Insert variables.
+		$( '.sui-select[data-theme="vars"]:not([data-search="true"])' ).SUIselect2({
+			templateResult: formatVars,
+			templateSelection: formatVarsSelection,
+			escapeMarkup: function( markup ) {
+				return markup;
+			},
+			minimumResultsForSearch: Infinity
+		});
+
+		// Insert variables.
+		$( '.sui-select[data-theme="vars"][data-search="true"]' ).SUIselect2({
+			templateResult: formatVars,
+			templateSelection: formatVarsSelection,
+			escapeMarkup: function( markup ) {
+				return markup;
+			}
+		});
+
+		// Smart search.
+		$( '.sui-select[data-theme="search"]' ).SUIselect2({
 			minimumInputLength: 2,
-			maximumSelectionLength: 1,
-            dropdownCssClass: 'sui-search-dropdown'
+			maximumSelectionLength: 1
 		});
-		$( '.sui-variables' ).SUIselect2({
-            dropdownCssClass: 'sui-variables-dropdown'
-		});
-    }
+	}
+
+	// Convert all select lists to fancy sui Select lists.
+    // if ( $( '.sui-color-accessible' )[0]) {
+
+    //     $( '.sui-select' ).SUIselect2({
+    //         dropdownCssClass: 'sui-select-dropdown sui-color-accessible'
+	// 	});
+
+	// 	$( '.sui-search' ).SUIselect2({
+	// 		minimumInputLength: 2,
+	// 		maximumSelectionLength: 1,
+    //         dropdownCssClass: 'sui-search-dropdown sui-color-accessible'
+	// 	});
+
+	// 	$( '.sui-variables' ).SUIselect2({
+    //         dropdownCssClass: 'sui-variables-dropdown sui-color-accessible'
+	// 	});
+    // } else {
+
+    //     $( '.sui-select' ).SUIselect2({
+	// 		templateResult: formatOption,
+	// 		templateSelection: formatOptionSelection,
+	// 		escapeMarkup: function( markup ) {
+	// 			return markup;
+	// 		}
+	// 	});
+
+	// 	$( '.sui-search' ).SUIselect2({
+	// 		minimumInputLength: 2,
+	// 		maximumSelectionLength: 1,
+    //         dropdownCssClass: 'sui-search-dropdown'
+	// 	});
+
+	// 	$( '.sui-variables' ).SUIselect2({
+    //         dropdownCssClass: 'sui-variables-dropdown'
+	// 	});
+    // }
 
 }( jQuery ) );
