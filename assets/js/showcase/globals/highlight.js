f74f8341957c1da14f3f53cc7863d389b3120f60
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-09-23 00:41:04 -0500

    [update] [showcase] improve demo component.

--- assets/js/showcase/globals/highlight.js
+++ assets/js/showcase/globals/highlight.js
@@ -7,6 +7,32 @@
 			hljs.highlightBlock( block );
 		});
 
+		$( '.showcase-component' ).each( function() {
+
+			var self = $( this );
+
+			if ( 150 < self.find( '.sui-code-snippet' ).outerHeight( true ) ) {
+
+				self.find( '.showcase-component-code--more' ).on( 'click', function() {
+
+					if ( self.find( '.showcase-component-code' ).hasClass( 'open' ) ) {
+						self.find( '.showcase-component-code' ).removeClass( 'open' );
+						self.find( '.showcase-component-code--more' ).html(
+							'Show More' +
+							'<span class="sui-icon-chevron-down" aria-hidden="true"></span>'
+						);
+					} else {
+						self.find( '.showcase-component-code' ).addClass( 'open' );
+						self.find( '.showcase-component-code--more' ).html(
+							'Show Less' +
+							'<span class="sui-icon-chevron-up" aria-hidden="true"></span>'
+						);
+					}
+				});
+			} else {
+				self.find( '.showcase-component-code--more' ).hide();
+			}
+		});
 	});
 
 }( jQuery ) );
