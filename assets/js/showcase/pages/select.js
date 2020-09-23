01589aa47e94e36ff0cf07380b8daf6aa5a75ec4
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-09-23 00:51:19 -0500

    [update] [select2] functions to show select2 options.

--- assets/js/showcase/pages/select.js
+++ assets/js/showcase/pages/select.js
@@ -18,7 +18,7 @@
 			return;
 		}
 
-		function demoOptions( select ) {
+		function singleSelect( select ) {
 
 			var container = select.closest( '.showcase-component' ),
 				options   = container.find( '.showcase-component-options' ),
@@ -151,10 +151,146 @@
 			});
 		}
 
+		function multiSelect( select ) {
+
+			var container = select.closest( '.showcase-component' ),
+				options   = container.find( '.showcase-component-options' ),
+				option    = options.find( 'fieldset' ),
+				searchbar
+				;
+
+			option.each( function() {
+
+				self = $( this );
+
+				if ( 'states' === $( this ).data( 'option' ) ) {
+
+					option = $( this ).find( 'input[type="radio"]' );
+
+					option.on( 'click', function() {
+
+						if ( 'disabled' === $( this ).val() ) {
+							select.attr( 'disabled', true );
+						} else {
+							select.attr( 'disabled', false );
+							select.removeAttr( 'disabled' );
+						}
+
+						if ( 'error' === $( this ).val() ) {
+							select.closest( '.sui-form-field' ).addClass( 'sui-form-field-error' );
+							select.closest( '.sui-form-field' ).find( '.sui-error-message' ).removeAttr( 'hidden' );
+							select.closest( '.sui-form-field' ).find( '.sui-error-message' ).removeClass( 'sui-hidden' );
+							select.closest( '.sui-form-field' ).find( '.sui-error-message' ).text( 'This field is required. Please, pick an option before continuing.' );
+						} else {
+							select.closest( '.sui-form-field' ).removeClass( 'sui-form-field-error' );
+							select.closest( '.sui-form-field' ).find( '.sui-error-message' ).attr( 'hidden' );
+							select.closest( '.sui-form-field' ).find( '.sui-error-message' ).addClass( 'sui-hidden' );
+							select.closest( '.sui-form-field' ).find( '.sui-error-message' ).empty();
+						}
+					});
+				}
+
+				if ( 'options' === $( this ).data( 'option' ) ) {
+
+					option = $( this ).find( 'input[type="checkbox"]' );
+
+					option.on( 'click', function() {
+
+						searchbar = ( self.find( '#multi-select-option--search' ).is( ':checked' ) ) ? true : false;
+
+						if ( $( this ).is( ':checked' ) ) {
+
+							// Add placeholder.
+							if ( 'placeholder' === $( this ).val() ) {
+
+								options = select.children().clone();
+
+								select
+									.attr( 'data-placeholder', 'Select Option' )
+									.empty()
+									.append( '<option></option>' )
+									.append( options )
+									;
+
+								if ( searchbar ) {
+
+									select
+										.SUIselect2( 'destroy' )
+										.SUIselect2()
+										;
+								} else {
+
+									select
+										.SUIselect2( 'destroy' )
+										.SUIselect2({
+											minimumResultsForSearch: -1
+										})
+										;
+								}
+							}
+
+							// Add search bar.
+							if ( 'search' === $( this ).val() ) {
+
+								select
+									.SUIselect2( 'destroy' )
+									.SUIselect2()
+									;
+							}
+						} else {
+
+							// Remove placeholder.
+							if ( 'placeholder' === $( this ).val() ) {
+
+								select
+									.removeAttr( 'data-placeholder' )
+									.find( 'option' )
+									.filter( function() {
+										return ! this.value || 0 === $.trim( this.value ).length;
+									})
+									.remove()
+									;
+
+								if ( searchbar ) {
+
+									select
+										.SUIselect2( 'destroy' )
+										.SUIselect2()
+										;
+								} else {
+
+									select
+										.SUIselect2( 'destroy' )
+										.SUIselect2({
+											minimumResultsForSearch: -1
+										})
+										;
+								}
+							}
+
+							// Remove search bar.
+							if ( 'search' === $( this ).val() ) {
+
+								select
+									.SUIselect2( 'destroy' )
+									.SUIselect2({
+										minimumResultsForSearch: -1
+									})
+									;
+							}
+						}
+					});
+				}
+			});
+		}
+
 		function init() {
 
 			// DEMO: Single Select.
-			demoOptions( $( '#single-select-demo' ) );
+			singleSelect( $( '#single-select-demo' ) );
+
+			// DEMO: Multi Select.
+			multiSelect( $( '#multi-select-demo' ) );
 
 		}
 
