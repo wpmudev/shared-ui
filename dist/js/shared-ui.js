764a91d424e9b2e772ebb4aa1f54c0520e09d32a
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-10-19 09:36:16 -0500

    📦 Shared UI v2.10.0-beta.8

--- dist/js/shared-ui.js
+++ dist/js/shared-ui.js
@@ -118,8 +118,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     return this;
   };
 
-  if (0 !== $('.sui-2-10-0-beta-7 .sui-accordion').length) {
-    $('.sui-2-10-0-beta-7 .sui-accordion').each(function () {
+  if (0 !== $('.sui-2-10-0-beta-8 .sui-accordion').length) {
+    $('.sui-2-10-0-beta-8 .sui-accordion').each(function () {
       SUI.suiAccordion(this);
     });
   }
@@ -233,7 +233,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   SUI.suiCodeSnippet = function () {
     // Convert all code snippet.
-    $('.sui-2-10-0-beta-7 .sui-code-snippet:not(.sui-no-copy)').each(function () {
+    $('.sui-2-10-0-beta-8 .sui-code-snippet:not(.sui-no-copy)').each(function () {
       // backward compat of instantiate new accordion
       $(this).SUICodeSnippet({});
     });
@@ -509,7 +509,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     return this;
   };
 
-  $('.sui-2-10-0-beta-7 .sui-slider').each(function () {
+  $('.sui-2-10-0-beta-8 .sui-slider').each(function () {
     SUI.dialogSlider(this);
   });
 })(jQuery);
@@ -525,7 +525,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   SUI.linkDropdown = function () {
     function closeAllDropdowns($except) {
-      var $dropdowns = $('.sui-2-10-0-beta-7 .sui-dropdown');
+      var $dropdowns = $('.sui-2-10-0-beta-8 .sui-dropdown');
 
       if ($except) {
         $dropdowns = $dropdowns.not($except);
@@ -546,7 +546,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
       e.preventDefault();
     });
     $('body').mouseup(function (e) {
-      var $anchor = $('.sui-2-10-0-beta-7 .sui-dropdown-anchor');
+      var $anchor = $('.sui-2-10-0-beta-8 .sui-dropdown-anchor');
 
       if (!$anchor.is(e.target) && 0 === $anchor.has(e.target).length) {
         closeAllDropdowns();
@@ -2501,7 +2501,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   }
 
   SUI.showHidePassword = function () {
-    $('.sui-2-10-0-beta-7 .sui-form-field').each(function () {
+    $('.sui-2-10-0-beta-8 .sui-form-field').each(function () {
       var $this = $(this);
 
       if (0 !== $this.find('input[type="password"]').length) {
@@ -2529,7 +2529,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 (function ($) {
   var endpoint = 'https://api.reviews.co.uk/merchant/reviews?store=wpmudev-org'; // Update the reviews with the live stats.
 
-  $('.sui-2-10-0-beta-7 .sui-reviews').each(function () {
+  $('.sui-2-10-0-beta-8 .sui-reviews').each(function () {
     var review = $(this);
     $.get(endpoint, function (data) {
       var stars = Math.round(data.stats.average_rating);
@@ -2567,7 +2567,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     $(el).prepend(svg).addClass('loaded').find('circle:last-child').css('animation', 'sui' + score + ' 3s forwards');
   };
 
-  $('.sui-2-10-0-beta-7 .sui-circle-score').each(function () {
+  $('.sui-2-10-0-beta-8 .sui-circle-score').each(function () {
     SUI.loadCircleScore(this);
   });
 })(jQuery);
@@ -8482,7 +8482,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     });
   };
 
-  $('.sui-2-10-0-beta-7 .sui-side-tabs label.sui-tab-item input').each(function () {
+  $('.sui-2-10-0-beta-8 .sui-side-tabs label.sui-tab-item input').each(function () {
     SUI.sideTabs(this);
   });
 })(jQuery);
@@ -8921,12 +8921,12 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     return this;
   };
 
-  if (0 !== $('.sui-2-10-0-beta-7 .sui-tabs').length) {
+  if (0 !== $('.sui-2-10-0-beta-8 .sui-tabs').length) {
     // Support tabs new markup.
     SUI.tabs(); // Support legacy tabs.
 
     SUI.suiTabs();
-    $('.sui-2-10-0-beta-7 .sui-tabs-navigation').each(function () {
+    $('.sui-2-10-0-beta-8 .sui-tabs-navigation').each(function () {
       SUI.tabsOverflow($(this));
     });
   }
@@ -9246,8 +9246,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     return this;
   };
 
-  if (0 !== $('.sui-2-10-0-beta-7 .sui-tree').length) {
-    $('.sui-2-10-0-beta-7 .sui-tree').each(function () {
+  if (0 !== $('.sui-2-10-0-beta-8 .sui-tree').length) {
+    $('.sui-2-10-0-beta-8 .sui-tree').each(function () {
       SUI.suiTree($(this), true);
     });
   }
@@ -9263,7 +9263,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   }
 
   SUI.upload = function () {
-    $('.sui-2-10-0-beta-7 .sui-upload-group input[type="file"]').on('change', function (e) {
+    $('.sui-2-10-0-beta-8 .sui-upload-group input[type="file"]').on('change', function (e) {
       var file = $(this)[0].files[0],
           message = $(this).find('~ .sui-upload-message');
 
