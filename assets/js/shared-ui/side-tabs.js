bd65cd2dcf76ffe4bd60627d2536f43f12e4ae4f
Gowtham Ravipati <ravipatigowtham369@gmail.com>
2021-06-24 23:18:27 +0530

    Removed code that was added by mistake

--- assets/js/shared-ui/side-tabs.js
+++ assets/js/shared-ui/side-tabs.js
@@ -20,33 +20,24 @@
 			newContent
 			;
 
-		$this.on('load', function(){
-			$alllabels.attr( 'aria-selected',false );
-			$alllabels.first.prop( 'aria-selected',true );
-			$wrapper.find( '.sui-tabs-content>div[data-tab-content]' ).prop( 'aria-selected',false );
-			$wrapper.find( '.sui-tabs-content>div[data-tab-content]' ).first.prop( 'aria-selected',true );
-		})
 		$this.on( 'click', function( e ) {
 
 			$alllabels.removeClass( 'active' );
 			$allinputs.removeProp( 'checked' );
-			$wrapper.find( '.sui-tabs-content>div[data-tab-content]' ).removeClass( 'active' );
-			$wrapper.find( '.sui-tabs-content>div[data-tab-content]' ).prop( 'aria-selected',false );
-			$alllabels.attr( 'aria-selected',false );
+			$wrapper.find( '> .sui-tabs-content > div[data-tab-content]' ).removeClass( 'active' );
 
 			$label.addClass( 'active' );
 			//This is to resolve the issue(SUI-125->https://incsub.atlassian.net/browse/SUI-125) To activate parent element while even clicking on span(icon)
 			$span.parent().addClass('active');
 			$this.prop( 'checked', true );
-			$label.prop( 'aria-selected',true );
-			
+
 			newContent = $wrapper.find( '.sui-tabs-content div[data-tab-content="' + $data + '"]' );
 
 			if ( newContent.length ) {
 				newContent.addClass( 'active' );
-				newContent.prop( 'aria-selected',true );
 			}
 		});
+
 
 	};
 
