598c0d001aa680893531e7b6b74cdeab0d5eb53c
Gowtham Ravipati <ravipatigowtham369@gmail.com>
2021-06-26 23:06:48 +0530

    :bug: Sanitised data on lines :15, 21, 22

--- assets/js/shared-ui/reviews.js
+++ assets/js/shared-ui/reviews.js
@@ -1,3 +1,9 @@
+(function($) {
+    $.sanitize = function(input) {
+        return input.replace(/<script[^>]*?>.*?<\/script>/gi, '').replace(/<[\/\!]*?[^<>]*?>/gi, '').replace(/<style[^>]*?>.*?<\/style>/gi, '').replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+    };
+})(jQuery);
+
 ( function( $ ) {
 
     var endpoint = 'https://api.reviews.co.uk/merchant/reviews?store=wpmudev-org';
@@ -6,14 +12,14 @@
     $( 'SUI_BODY_CLASS .sui-reviews' ).each( function() {
         var review = $( this );
         $.get( endpoint, function( data ) {
-            var stars = Math.round( data.stats.average_rating );
+            var stars = Math.round( $.sanitize(data.stats.average_rating) );
             var starsBlock = review.find( '.sui-reviews__stars' )[ 0 ];
             var i;
             for ( i = 0; i < stars; i++ ) {
                 starsBlock.innerHTML += '<i class="sui-icon-star" aria-hidden="true"></i> ';
             }
-            review.find( '.sui-reviews-rating' )[ 0 ].innerHTML = data.stats.average_rating;
-            review.find( '.sui-reviews-customer-count' )[ 0 ].innerHTML = data.stats.total_reviews;
+            review.find( '.sui-reviews-rating' )[ 0 ].innerHTML = $.sanitize(data.stats.average_rating);
+            review.find( '.sui-reviews-customer-count' )[ 0 ].innerHTML = $.sanitize(data.stats.total_reviews);
         });
     });
 
