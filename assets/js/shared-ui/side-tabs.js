b34e58d1ef8e6f45d71aa05a46d76fa7a4626c0c
Gowtham Ravipati <ravipatigowtham369@gmail.com>
2021-06-26 22:12:39 +0530

    :bug: added pointer events none to make all the child elements non clickable

--- assets/js/shared-ui/side-tabs.js
+++ assets/js/shared-ui/side-tabs.js
@@ -12,14 +12,15 @@
 
 		var $this 	   = $( element ),
 			$label     = $this.parent( 'label' ),
-			$span     = $this.parent( 'label > span' ),
 			$data      = $this.data( 'tab-menu' ),
 			$wrapper   = $this.closest( '.sui-side-tabs' ),
 			$alllabels = $wrapper.find( '>.sui-tabs-menu .sui-tab-item' ),
 			$allinputs = $alllabels.find( 'input' ),
 			newContent
 			;
-
+		$(window).on('load', function(){
+			$alllabels.children().css("pointer-events","none")
+		})
 		$this.on( 'click', function( e ) {
 
 			$alllabels.removeClass( 'active' );
@@ -28,7 +29,6 @@
 
 			$label.addClass( 'active' );
 			//This is to resolve the issue(SUI-125->https://incsub.atlassian.net/browse/SUI-125) To activate parent element while even clicking on span(icon)
-			$span.parent().addClass('active');
 			$this.prop( 'checked', true );
 
 			newContent = $wrapper.find( '.sui-tabs-content div[data-tab-content="' + $data + '"]' );
