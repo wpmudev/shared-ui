1b090c2992fe58de0d788f58fe5fc5effa127a35
Pawan Kumar <pwnkumar201@gmail.com>
2021-10-08 13:28:07 +0530

    Upload file function isn't working - [SUI-238]

--- assets/js/shared-ui/upload.js
+++ assets/js/shared-ui/upload.js
@@ -24,24 +24,23 @@
 		$('.sui-upload-button').on('click', function(){
 			var parent = $(this).parent();
 			var file = parent.find('input[type="file"]');
-			
-			// This will open browser file upload explorer
+			// This will open browser file explorer
 			file.trigger('click');
 		})
 
-		// This will trigger after file uploads. 
+		// This will trigger after selecting file for upload. 
 		$('.sui-upload input[type="file"]').on( 'change', function() {
-			var filename = $(this).val();
 			var parent = $(this).parent();
+			var filename = $(this).val();
 			var imageContainer = parent.find('.sui-upload-image');
 			var lastIndex = filename.lastIndexOf("\\");
 			if (lastIndex >= 0) {
 				filename = filename.substring(lastIndex + 1);
 			}
+			// To show uploaded file preview.
 			if(imageContainer.length){
 				var reader = new FileReader();
 				var imagePreview = imageContainer.find('.sui-image-preview');
-				// var imageSrc = URL.createObjectURL(filename);
 				reader.onload = function (e) {
 					imagePreview.attr('style', 'background-image: url('+e.target.result+');');
 				}
