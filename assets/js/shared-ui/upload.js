cf7ba5d386eebe8e7173428cbe41f3282e30eadc
Pawan Kumar <pwnkumar201@gmail.com>
2021-10-14 01:13:56 +0530

    Added drag and drop to upload files.

--- assets/js/shared-ui/upload.js
+++ assets/js/shared-ui/upload.js
@@ -22,27 +22,16 @@
 
 		// check whether element exist then execute js
 		if($( 'SUI_BODY_CLASS .sui-file-upload' ).length){
+			
 			// This will trigger on file change. 
 			$( 'SUI_BODY_CLASS .sui-file-upload input[type="file"]' ).on( 'change', function() {
-				var parent = $( this ).parent();
 				var filename = $( this ).val();
+				var lastIndex = filename.lastIndexOf( "\\" );
+				var parent = $( this ).closest( '.sui-upload' );
 				var imageContainer = parent.find( '.sui-upload-image' );
-				if(filename) {
-					var lastIndex = filename.lastIndexOf( "\\" );
-					if ( lastIndex >= 0 ) {
-						filename = filename.substring(lastIndex + 1);
-						// To show uploaded file preview.
-						if( imageContainer.length ){
-							var reader = new FileReader();
-							var imagePreview = imageContainer.find( '.sui-image-preview' );
-							reader.onload = function ( e ) {
-								imagePreview.attr( 'style', 'background-image: url('+e.target.result+' );' );
-							}
-							reader.readAsDataURL($( this )[0].files[0]);
-						}
-						parent.find( '.sui-upload-file > span' ).text(filename);
-						parent.addClass( 'sui-has_file' );
-					}
+				if ( lastIndex >= 0 ) {
+					filename = filename.substring(lastIndex + 1);
+					uploadedFile( $( this ), $( this )[0].files[0], filename);
 				} else {
 					if( imageContainer.length ){
 						var imagePreview = imageContainer.find( '.sui-image-preview' );
@@ -50,7 +39,7 @@
 					}
 					parent.find( '.sui-upload-file > span' ).text( '' );
 					parent.removeClass( 'sui-has_file' );
-				}		
+				}	
 			});
 
 			// This will trigger on click of upload button
@@ -67,6 +56,51 @@
 			$( 'SUI_BODY_CLASS .sui-file-upload .sui-upload-image' ).on( 'click', function(){
 				selectFile($( this ));
 			});
+
+			// upload drag and drop functionality
+			var isAdvancedUpload = function() {
+				var div = document.createElement('div');
+				return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+			}();
+
+			var uploadArea = $( 'SUI_BODY_CLASS .sui-upload-button' );
+
+			if(isAdvancedUpload){
+				var droppedFiles = false;
+
+				uploadArea.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+					e.preventDefault();
+					e.stopPropagation();
+				})
+				.on('dragover dragenter', function() {
+					uploadArea.addClass('sui-is-dragover');
+				})
+				.on('dragleave dragend drop', function() {
+					uploadArea.removeClass('sui-is-dragover');
+				})
+				.on('drop', function(e) {
+					droppedFiles = e.originalEvent.dataTransfer.files;
+					uploadedFile( $( this ), droppedFiles[0], droppedFiles[0].name);
+				});
+			}
+
+			// function to set uploaded file
+			function uploadedFile(element, file, filename){
+				var parent = element.closest( '.sui-upload' );
+				var imageContainer = parent.find( '.sui-upload-image' );
+				if(filename) {
+					if( imageContainer.length ){
+						var reader = new FileReader();
+						var imagePreview = imageContainer.find( '.sui-image-preview' );
+						reader.onload = function ( e ) {
+							imagePreview.attr( 'style', 'background-image: url('+e.target.result+' );' );
+						}
+						reader.readAsDataURL(file);
+					}
+					parent.find( '.sui-upload-file > span' ).text(filename);
+					parent.addClass( 'sui-has_file' );
+				}
+			}
 
 			// function to open browser file explorer for selecting file
 			function selectFile(element) {
