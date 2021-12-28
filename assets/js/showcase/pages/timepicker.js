f1183bd69d097f1edcc374a4c1ed7a695515e7f4
Pawan Kumar <pwnkumar201@gmail.com>
2021-12-28 05:34:25 +0530

    added timepicker widget

---
+++ assets/js/showcase/pages/timepicker.js
@@ -0,0 +1,53 @@
+( function( $ ) {
+
+	// Use strict mode
+	'use strict';
+
+	// Define global DEMO object if it does not exist
+	if ( 'object' !== typeof window.DEMO ) {
+		window.DEMO = {};
+	}
+
+	DEMO.pageTimePicker = function( page ) {
+
+		var body = $( 'body' ),
+			page = 'showcase-page-' + page
+			;
+
+		if ( page !== body.attr( 'id' ) ) {
+			return;
+		}
+
+		function timePicker( element ) {
+
+			element = $( element );
+
+			element.clockTimePicker({
+				duration: true,
+				precision: 1,
+				vibrate: true,
+				required: true
+
+			});
+		}
+
+		function init() {
+
+			timePicker( '#time-simple-default' );
+			timePicker( '#time-simple-error' );
+
+		}
+
+		init();
+
+		return this;
+
+	};
+
+	$( 'body' ).ready( function() {
+
+		DEMO.pageTimePicker( 'timepicker' );
+
+	});
+
+}( jQuery ) );
