9131def9b0a898218bc5bf42cd6b2a62ae5ec8ae
Pawan Kumar <pwnkumar201@gmail.com>
2022-02-11 13:20:31 +0530

    ✨ enhancement(uploads): hoisting of function declared in blocks

--- assets/js/shared-ui/upload.js
+++ assets/js/shared-ui/upload.js
@@ -8,39 +8,38 @@
 		window.SUI = {};
 	}
 
-	// function to set uploaded file
-	const uploadedFile = function (element, file, filename){
-		var parent = element.closest( '.sui-upload' );
-		var imageContainer = parent.find( '.sui-upload-image' );
-		if(filename) {
-			if( imageContainer.length ){
-				var reader = new FileReader();
-				var imagePreview = imageContainer.find( '.sui-image-preview' );
-				reader.onload = function ( e ) {
-					imagePreview.attr( 'style', 'background-image: url('+e.target.result+' );' );
-				}
-				reader.readAsDataURL(file);
-			}
-			parent.find( '.sui-upload-file > span' ).text(filename);
-			parent.addClass( 'sui-has_file' );
-		}
-	}
-
-	// function to open browser file explorer for selecting file
-	const selectFile = function (element) {
-		var parent = element.closest( '.sui-upload' );
-		var file = parent.find( 'input[type="file"]' );
-		file.trigger( 'click' );
-	}
-
-	// function to remove file
-	const removeFile = function (element) {
-		var parent = element.closest( '.sui-upload' );
-		var file = parent.find( 'input[type="file"]' );
-		file.val('').change();
-	}
-
 	SUI.upload = function() {
+		// function to set uploaded file
+		const uploadedFile = function (element, file, filename){
+			var parent = element.closest( '.sui-upload' );
+			var imageContainer = parent.find( '.sui-upload-image' );
+			if(filename) {
+				if( imageContainer.length ){
+					var reader = new FileReader();
+					var imagePreview = imageContainer.find( '.sui-image-preview' );
+					reader.onload = function ( e ) {
+						imagePreview.attr( 'style', 'background-image: url('+e.target.result+' );' );
+					}
+					reader.readAsDataURL(file);
+				}
+				parent.find( '.sui-upload-file > span' ).text(filename);
+				parent.addClass( 'sui-has_file' );
+			}
+		}
+
+		// function to open browser file explorer for selecting file
+		const selectFile = function (element) {
+			var parent = element.closest( '.sui-upload' );
+			var file = parent.find( 'input[type="file"]' );
+			file.trigger( 'click' );
+		}
+
+		// function to remove file
+		const removeFile = function (element) {
+			var parent = element.closest( '.sui-upload' );
+			var file = parent.find( 'input[type="file"]' );
+			file.val('').change();
+		}
 
 		$( 'SUI_BODY_CLASS .sui-upload-group input[type="file"]' ).on( 'change', function( e ) {
 			var file = $( this )[0].files[0],
