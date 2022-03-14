fa0474e572e1c83e1bea20f72dc98d81c08a54fc
Pawan Kumar <pwnkumar201@gmail.com>
2022-03-14 17:02:54 +0530

    made requested changes

--- assets/js/shared-ui/select2.js
+++ assets/js/shared-ui/select2.js
@@ -268,4 +268,10 @@
 		SUI.select.initVars( select );
 
 	});
+
+	// add accessible class to the select.
+	if ( $( '.sui-color-accessible' ).length ) {
+		$( 'body' ).addClass( 'sui-select-accessible' );
+	}
+
 }( jQuery ) );
