b0247d908503e001404fdcebfb7fef4a80efd3da
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-06-21 14:25:12 -0500

    👷 fix(compiler): Clean `dist` folder.
    Remove all files inside `dist` folder before compiling a new version.

--- storybook/packages/sui-compiler/lib/sui-compiler.js
+++ storybook/packages/sui-compiler/lib/sui-compiler.js
@@ -1,7 +1,64 @@
-'use strict';
+#!/usr/bin/env node
 
-module.exports = suiCompiler;
+/**
+ * @license
+ * Copyright (C) 2022 Incsub LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * THIS PROGRAM IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL, BUT WITHOUT
+ * ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR
+ * FITNESSFOR A PARTICULAR PURPOSE.
+ *
+ * SEE THE GNU GENERAL PUBLIC LICENSE FOR MORE DETAILS.
+ */
 
-function suiCompiler() {
-	// TODO
+const rollup = require('rollup');
+const path = require('path');
+const del = require('rollup-plugin-delete');
+
+const currentWorkingPath = process.cwd();
+const { main, name } = require(path.join(currentWorkingPath, 'package.json'));
+
+const inputPath = path.join(currentWorkingPath, main);
+
+// Little workaround to get package name without scope.
+const fileName = name.replace('@wpmudev/', '');
+
+// See below for details on the options.
+const inputOptions = {
+	input: inputPath,
+	plugins: [
+		del({
+			targets: ['dist/*'],
+			verbose: true,
+		}),
+	],
+};
+
+const outputOptions = [
+	{
+		file: `dist/${fileName}.cjs.js`,
+		format: 'cjs',
+	},
+];
+
+async function build() {
+	// Initial message.
+	console.log('👷 Preparing to build packages.');
+
+	// Create bundle.
+	const bundle = await rollup.rollup(inputOptions);
+
+	// Loop through the options and write individual bundles.
+	outputOptions.forEach(async (options) => {
+		await bundle.write(options);
+	});
+
+	// Final message.
+	console.log('📦️ Finished building packages.');
 }
+
+build();
