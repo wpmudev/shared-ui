521b6a1fa80c81185e9fcf901126ff221a3bf8f8
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-06-21 20:03:56 -0500

    👷 Install Storybook.
    Use html framework to build Storybook and point stories to packages for better organization.

---
+++ storybook/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+	stories: [
+		'../packages/**/*.stories.mdx',
+		'../packages/**/*.stories.@(js|jsx|ts|tsx)',
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+	],
+	framework: '@storybook/html',
+};
