f5d928d9a9681bf889ac65eeebeee923afefa435
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-06-21 22:38:37 -0500

    🧑‍💻 fix(storybook): Set viewport breakpoints.

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,3 +1,50 @@
+/**
+ * Get devices samples from:
+ * https://screensiz.es/
+ */
+const breakpoints = {
+	xl: {
+		name: 'Dell UltraSharp 24"',
+		styles: {
+			width: '1920px', // Min 1600px
+			height: '1200px',
+		},
+		type: 'extra-large',
+	},
+	lg: {
+		name: 'MacBook Air 13"',
+		styles: {
+			width: '1440px', // Min 1200px - Max 1599px
+			height: '900px',
+		},
+		type: 'laptop',
+	},
+	md: {
+		name: 'Asus ZenBook',
+		styles: {
+			width: '1080px', // Min 1024px - Max 1199px
+			height: '1920px',
+		},
+		type: 'tablet',
+	},
+	sm: {
+		name: 'Nokia Lumia',
+		styles: {
+			width: '768px', // Min 783px - Max 1023px
+			height: '1280px',
+		},
+		type: 'mobile',
+	},
+	xs: {
+		name: 'iPhone SE',
+		styles: {
+			width: '640px', // Min 0 - Max 782px
+			height: '1136px',
+		},
+		type: 'smaller',
+	},
+};
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	controls: {
@@ -5,6 +52,9 @@ export const parameters = {
 			color: /(background|color)$/i,
 			date: /Date$/,
 		},
+	},
+	viewport: {
+		viewports: breakpoints,
 	},
 	previewTabs: {
 		'storybook/docs/panel': {
