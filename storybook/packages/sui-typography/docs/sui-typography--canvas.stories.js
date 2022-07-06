97997b17663db59bb74faa5ff0b7489da150a5a0
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-07-06 00:06:33 -0500

    ⚗️ new(sui-utilities): Add test content for stories.

---
+++ storybook/packages/sui-typography/docs/sui-typography--canvas.stories.js
@@ -0,0 +1,18 @@
+import docs from './sui-typography--docs.mdx';
+
+export default {
+	title: 'SUI/Components/Typography',
+	parameters: {
+		docs: {
+			page: docs,
+		},
+	},
+};
+
+export const Primary = () => {
+	return `<p>Hello</p>`;
+};
+
+export const Secondary = () => {
+	return `<p>Hello 2</p>`;
+};
