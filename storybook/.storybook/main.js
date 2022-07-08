3a0cb2453c996c2dc9d055dc981d7cf5f5b64576
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-07-07 22:14:20 -0500

    🐛 fix(core): Unexpected character ‘@‘.

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
 	stories: [
 		'../packages/**/*.stories.mdx',
@@ -9,6 +11,16 @@ module.exports = {
 		'@storybook/addon-interactions',
 		'@storybook/preset-scss',
 	],
+	webpackFinal: async (config, { configType }) => {
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: ['style-loader', 'css-loader', 'sass-loader'],
+			include: path.resolve(__dirname, './css/'),
+		});
+
+		// Return the altered configs.
+		return config;
+	},
 	framework: '@storybook/html',
 	core: {
 		builder: '@storybook/builder-webpack5',
