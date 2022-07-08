51d4dc9fe889c390d1e49e4d90a5553b9889fe8f
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-07-07 21:19:12 -0500

    ➕ Add Sb support for `SCSS` files.

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -7,6 +7,21 @@ module.exports = {
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-interactions',
+		{
+			name: '@storybook/preset-scss',
+			options: {
+				cssLoaderOptions: {
+					modules: true,
+					localIdentName: '[name]__[local]--[hash:base64:5]',
+				},
+			},
+		},
 	],
+	// webpackFinal: async (config) => {
+	// 	console.log(config.resolve);
+
+	// 	// Return the altered configs.
+	// 	return config;
+	// },
 	framework: '@storybook/html',
 };
